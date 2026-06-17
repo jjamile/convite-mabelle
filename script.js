6718d96bafcddbd4157da0e835a4511b4db2d57e
jjamile <124084261+jjamile@users.noreply.github.com>
2026-06-17 12:44:02 -0300

    Refactor functions for handling invitation and music

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
 function abrirConvite(){
 
 
-    document.getElementById("entrada").style.display = "none";
-
-
-    document.getElementById("conteudo").style.display = "block";
-
-
-    let musica = document.getElementById("musica");
-
-
-    musica.play()
-    .then(() => {
-
-        console.log("Música tocando");
-
-    })
-    .catch(() => {
-
-        console.log("Erro ao iniciar música");
-
-    });
+document.getElementById("entrada")
+.style.display="none";
 
 
 
-    document.getElementById("botaoMusica").style.display = "block";
+document.getElementById("convite")
+.style.display="block";
+
+
+
+let musica = document.getElementById("musica");
+
+
+musica.play();
+
+
+
+document.getElementById("botaoMusica")
+.style.display="block";
 
 
 }
@@ -36,29 +31,34 @@ function abrirConvite(){
 function controleMusica(){
 
 
-    let musica = document.getElementById("musica");
-
-    let botao = document.getElementById("botaoMusica");
-
+let musica =
+document.getElementById("musica");
 
 
-    if(musica.paused){
+let botao =
+document.getElementById("botaoMusica");
 
 
-        musica.play();
 
-        botao.innerHTML = "🔊 Música ON";
-
-
-    } else {
+if(musica.paused){
 
 
-        musica.pause();
-
-        botao.innerHTML = "🔇 Música OFF";
+musica.play();
 
 
-    }
+botao.innerHTML="🔊 Música ON";
+
+
+}else{
+
+
+musica.pause();
+
+
+botao.innerHTML="🔇 Música OFF";
+
+
+}
 
 
 }
@@ -67,14 +67,14 @@ function controleMusica(){
 
 
 
-
-
 function abrirMapa(){
 
 
-    window.open(
-    "https://maps.google.com/?q=Chácara+Oásis+Quixeramobim"
-    );
+window.open(
+
+"https://maps.google.com/?q=Chácara+Oásis+Quixeramobim"
+
+);
 
 
 }
@@ -88,24 +88,27 @@ function abrirMapa(){
 function confirmar(){
 
 
-    let nome =
-    document.getElementById("nome").value;
-
-
-    let quantidade =
-    document.getElementById("quantidade").value;
-
-
-    let resposta =
-    document.getElementById("resposta").value;
+let nome =
+document.getElementById("nome").value;
 
 
 
-    let numero = "558896468988";
+let quantidade =
+document.getElementById("quantidade").value;
 
 
 
-    let mensagem = 
+let resposta =
+document.getElementById("resposta").value;
+
+
+
+let numero="5588996468988";
+
+
+
+let mensagem=
+
 `Olá! 💚✨
 
 Confirmação do aniversário da Mabelle - 15 anos
@@ -117,20 +120,15 @@ Quantidade:
 ${quantidade}
 
 Resposta:
-${resposta}
-`;
+${resposta}`;
 
 
 
-    let link =
-    "https://wa.me/" 
-    + numero 
-    + "?text=" 
-    + encodeURIComponent(mensagem);
+window.open(
 
+"https://wa.me/"+numero+"?text="+encodeURIComponent(mensagem)
 
-
-    window.open(link, "_blank");
+);
 
 
 }
